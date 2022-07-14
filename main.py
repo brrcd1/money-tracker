16efc715a622d771c74bf9c1870f105983189c99
Timur <notu0179@gmail.com>
2022-07-14 14:57:59 +0500

    add comments(#)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import datetime
 import time
 
-now = datetime.datetime.now().replace(microsecond=0)
+now = datetime.datetime.now().replace(microsecond=0) # Убираем милисекунды с даты
 deposit = float(input('Сколько денег тебе пришло?'))
 cost = float(input('Сколько денег тебе нужно потратить на важные расходы?'))
 balance = deposit - cost
 print('Столько денег осталось на различные плюшки:', balance)
-time.sleep(3)
-with open('uchet.txt', 'a') as file:
+time.sleep(3) # задержка с которой закроется консолька
+with open('uchet.txt', 'a') as file: # команда открытия файла, метод 'a' требуется для дозаписи
     file.write('\n')
     file.write(str(now))
     file.write(' | ')
